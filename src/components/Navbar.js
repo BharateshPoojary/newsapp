bf1e41e154d018f90727691160a991ef3482a533
Bharatesh Poojary <bharateshpoojari@gmail.com>
2023-10-01 15:31:09 +0530

    refactored navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,6 +41,14 @@ export default class Navbar extends Component {
             {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
             <a href="/" className="bg-gray-900 text-white rounded-md px-3 py-2 text-xl font-medium" aria-current="page">Home</a>
             <a href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-xl font-medium">About</a>
+            <a href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-xl font-medium">business</a>
+            <a href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-xl font-medium">entertainment</a>
+            <a href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-xl font-medium">general</a>
+            <a href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-xl font-medium">health</a>
+            <a href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-xl font-medium">science</a>
+            <a href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-xl font-medium">sports</a>
+            <a href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-xl font-medium">technology</a>
+
           </div>
         </div>
       </div>
--- src/components/News.js
+++ src/components/News.js
@@ -1,46 +1,69 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+
 
 export default class News extends Component {
+  static defaultProps={
+    pageSize:9,
+    country:'in',
+    category:"general"
+  }
+  static propTypes={
+    pageSize:PropTypes.number,
+    country:PropTypes.string,
+    category:PropTypes.string
+  }
   constructor(){
     super();
     this.state={
       articles:[],
-      page:1
+      page:1,
+      loading:false
     }
   }
    async componentDidMount(){
-    let url='https://newsapi.org/v2/top-headlines?country=in&apiKey=e69f73e7612741f09d9420c7c62384bf&page=1&pageSize=20';
+    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e69f73e7612741f09d9420c7c62384bf&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
     let data = await fetch(url);
     let parseddata=await data.json();
-    this.setState({articles:parseddata.articles,totalResults:parseddata.totalResults})
+    this.setState({articles:parseddata.articles,
+      totalResults:parseddata.totalResults,
+      loading:false
+    })
   }
   handleprevClick=async()=>{
     console.log("I am previous");
-    let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=e69f73e7612741f09d9420c7c62384bf&page=${this.state.page-1}&pageSize=20`;
+    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e69f73e7612741f09d9420c7c62384bf&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true})
     let data = await fetch(url);
     let parseddata=await data.json();
     this.setState({page:this.state.page-1,
-    articles:parseddata.articles})
+    articles:parseddata.articles,
+    loading:false
+  })
   }
   handlenextClick=async()=>{
-    console.log("I am Next");
-if (this.state.page+1>Math.ceil(this.state.totalResults/20)) {
-  // alert("i am last");
-}
- else{  
-   let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=e69f73e7612741f09d9420c7c62384bf&page=${this.state.page+1}&pageSize=20`;
-    let data = await fetch(url);
+    console.log("I am Next");  
+   let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e69f73e7612741f09d9420c7c62384bf&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+   this.setState({loading:true}) 
+   let data = await fetch(url);
     let parseddata=await data.json();
     this.setState({page:this.state.page+1,
-    articles:parseddata.articles})
-  }
+    articles:parseddata.articles,
+    loading:false
+  })
 }
   render() {
     return (
-      <>     
+      <>   
+      <div className="text-center text-4xl">TOP HEADLINES </div> 
+  
+      {this.state.loading && <Spinner/>} 
+      
        <div className='mt-5  grid grid-cols-3 gap-3'>
-      {this.state.articles.map((element)=>{
+      {!this.state.loading && this.state.articles.map((element)=>{
       console.log(element);
         return <div key={element.url}  className=' border-solid border-2 border-slate-400 rounded-lg'>
         <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url}/>
@@ -48,7 +71,8 @@ if (this.state.page+1>Math.ceil(this.state.totalResults/20)) {
   })}  
   </div>
  <div className="flex justify-between mt-2">
- <button disabled={this.state.page<=1}className='h-10 w-20 pb-2 pr-3 rounded-lg bg-black text-white hover:bg-white hover:text-black hover:border-solid hover:border-2 hover:border-slate-500'onClick={this.handleprevClick}>&larr;previous</button><button className='h-10 w-20 rounded-lg bg-black text-white hover:bg-white hover:text-black hover:border-solid hover:border-2 hover:border-slate-500'onClick={this.handlenextClick}>Next&rarr;</button> 
+ <button disabled={this.state.page<=1}className='h-10 w-20 pb-2 pr-3 rounded-lg bg-black text-white hover:bg-white hover:text-black hover:border-solid hover:border-2 hover:border-slate-500'onClick={this.handleprevClick}>&larr;previous</button>
+ <button disabled={this.state.page+1>Math.ceil(this.state.totalResults/this.props.pageSize)}className='h-10 w-20 rounded-lg bg-black text-white hover:bg-white hover:text-black hover:border-solid hover:border-2 hover:border-slate-500'onClick={this.handlenextClick}>Next&rarr;</button> 
  </div>  
  </>
     
